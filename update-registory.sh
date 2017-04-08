50e504165dcd75d08968485d6f434d001fa893c8
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2017-04-08 16:33:38 +0900

    add upload registory?

---
+++ update-registory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git config credential.helper "store --file=~/.git-credentials"
+echo "https://${GH_TOKEN}:@github.com" > ~/.git-credentials
+git clone https://github.com/kounoike/gitbucket-plugin-registry.git registry
+mkdir -p registry/$GITBUCKET_VERSION
+pushd registry
+echo "{\"name\": \"$PLUGIN_NAME\", \"version\": \"$PLUGIN_VERSION\", \"url\": \"$PLUGIN_URL\"}" > $GITBUCKET_VERSION/$TARGET_PLUGIN.json
+git add .
+git commit -m "CI Check passed for $PLUGIN_NAME:$PLUGIN_VERSION with GitBucket-$GITBUCKET_VERSION"
+git push origin HEAD:master
+popd
