e6b5b344dac31a4430e761da567b980caf28f186
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2017-04-08 16:05:04 +0900

    support multiple plugins by single checker repository.

--- src/test/scala/PluginTestSpec.scala
+++ src/test/scala/PluginTestSpec.scala
@@ -4,14 +4,16 @@ import org.scalatest.time.{Seconds, Span}
 
 class PluginTestSpec extends FlatSpec with Matchers with Chrome {
   val base = "http://localhost:8080"
+  val targetPlugin = System.getProperty("targetPlugin")
+  val targetVersion = System.getProperty("targetVersion")
   "plugins page" should "contains this plugin" in {
     go to base + "/signin?redirect=%2Fadmin%2Fplugins"
     textField("userName").value = "root"
     pwdField("password").value = "root"
     submit()
     implicitlyWait(Span(10, Seconds))
-    val ele = find(XPathQuery("""//a[@href="#ipynb"]"""))
+    val ele = find(XPathQuery(s"""//a[@href="#$targetPlugin"]"""))
     ele should not be None
-    ele.get.text should be ("ipynb:0.1.1")
+    ele.get.text should be (s"$targetPlugin:$targetVersion")
   }
 }
