e7d8b485f3c63ee1f10fed312f454e06a17a0a09
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2017-04-08 17:39:40 +0900

    registry files move to under registry directory.

--- update-registory.sh
+++ update-registory.sh
@@ -6,7 +6,7 @@ mkdir -p registry/$GITBUCKET_VERSION
 pushd registry
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-echo "{\"name\": \"$PLUGIN_NAME\", \"version\": \"$PLUGIN_VERSION\", \"url\": \"$PLUGIN_URL\"}" > $GITBUCKET_VERSION/$TARGET_PLUGIN.json
+echo "{\"name\": \"$PLUGIN_NAME\", \"version\": \"$PLUGIN_VERSION\", \"url\": \"$PLUGIN_URL\"}" > registry/$GITBUCKET_VERSION/$TARGET_PLUGIN.json
 git add .
 git commit -m "CI Check passed for $PLUGIN_NAME:$PLUGIN_VERSION with GitBucket-$GITBUCKET_VERSION"
 git push origin HEAD:master
