6cd668213884be6e473163d45d023f2675a758d2
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2017-04-08 17:43:40 +0900

    registry files move to under registry/plugins directory.

--- update-registory.sh
+++ update-registory.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 source plugins/$TARGET_PLUGIN
-git clone https://github.com/kounoike/gitbucket-plugin-registry.git registry
-mkdir -p registry/$GITBUCKET_VERSION
-pushd registry
+git clone https://github.com/kounoike/gitbucket-plugin-registry.git
+pushd gitbucket-plugin-registry
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-echo "{\"name\": \"$PLUGIN_NAME\", \"version\": \"$PLUGIN_VERSION\", \"url\": \"$PLUGIN_URL\"}" > registry/$GITBUCKET_VERSION/$TARGET_PLUGIN.json
+
+mkdir -p registry/plugins/$GITBUCKET_VERSION
+echo "{\"name\": \"$PLUGIN_NAME\", \"version\": \"$PLUGIN_VERSION\", \"url\": \"$PLUGIN_URL\"}" > registry/plugins/$GITBUCKET_VERSION/$TARGET_PLUGIN.json
 git add .
 git commit -m "CI Check passed for $PLUGIN_NAME:$PLUGIN_VERSION with GitBucket-$GITBUCKET_VERSION"
 git push origin HEAD:master
